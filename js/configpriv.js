943f5b8e5541ec0159b0e0899936963dee41ea63
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2024-10-13 17:47:14 -0500

    Update configpriv.js

--- js/configpriv.js
+++ js/configpriv.js
@@ -68,3 +68,17 @@ $(document).ready(function(){
     });
   });
 
+componente = document.getElementById('texto-services');
+const textoServicios = "Nuestros servicios";
+index = 0;
+
+funcion liveWriting (){
+  if(index < textoServicios.length){
+    componente += textoServicios[index];
+    index++;
+    setTimeout(liveWriting(),100);
+  }
+}
+
+liveWriting();
+
